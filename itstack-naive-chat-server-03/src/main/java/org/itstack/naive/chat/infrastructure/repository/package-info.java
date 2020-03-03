5921da86a464c2ef6275720cd3673eb4f99c5a9d
fuzhengwei <184172133@qq.com>
2020-03-03 09:43:28 +0800

    小傅哥 |《Netty+JavaFx实战：仿桌面版微信聊天》· 专栏Demo

---
+++ itstack-naive-chat-server-03/src/main/java/org/itstack/naive/chat/infrastructure/repository/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 微信公众号：bugstack虫洞栈 | 欢迎关注学习专题案例
+ * 论坛：http://bugstack.cn
+ * Create by 付政委 on @2019
+ */
+package org.itstack.naive.chat.infrastructure.repository;
---
+++ itstack-naive-chat-server-05/src/main/java/org/itstack/naive/chat/util/MsgUtil.java
@@ -0,0 +1,17 @@
+package org.itstack.naive.chat.util;
+
+import org.itstack.naive.chat.domain.MsgInfo;
+
+/**
+ * 虫洞栈：https://bugstack.cn
+ * 公众号：bugstack虫洞栈  ｛关注获取学习源码｝
+ * 虫洞群：①群5398358 ②群5360692
+ * Create by fuzhengwei on 2020
+ */
+public class MsgUtil {
+
+    public static MsgInfo buildMsg(String channelId, String msgContent) {
+        return new MsgInfo(channelId,msgContent);
+    }
+
+}
